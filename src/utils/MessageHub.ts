c05697c3a88ce28619d22106fa0aad66bf045392
Carlos Martinez <itscarlosisaac@gmail.com>
2019-12-17 17:00:54 -0400

    Created message system.

---
+++ src/utils/MessageHub.ts
@@ -0,0 +1,21 @@
+import EventEmitter from 'events';
+
+export const MessageHub = new EventEmitter();
+
+export enum EventType {
+  Toggle = 'toggle'
+}
+
+export interface IMessage {
+  eventType: string;
+}
+
+export interface IToogleMessage extends IMessage {
+  state: number;
+}
+
+export class MessageHubControllers {
+  static Toggle(data: any) {
+    MessageHub.emit(EventType.Toggle, data)
+  }
+}
