198a956e5d9672f1378511a6ff18606ff4f72b6a
Carlos Martinez <itscarlosisaac@gmail.com>
2020-01-02 18:01:43 -0400

    Fixed import option.

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { PeriodicTable } from "./components/PeriodicTable";
+import PeriodicTable from "./components/PeriodicTable";
 
 // import 'normalize.css';
 // TODO Add loader for normalize CSS
