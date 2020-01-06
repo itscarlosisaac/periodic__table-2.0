987f0bbeab7200722f5711566a83637b1c88de12
Carlos Martinez <itscarlosisaac@gmail.com>
2020-01-06 12:02:46 -0400

    Adde colors contants.

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,2 +1,18 @@
 export const APIURL =
   "https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json";
+
+export const ElementColors = {
+  'gas': '#7F3433',
+  'solid': '#596673',
+  'liquid': '#5EA8E3',
+  'unknown': '#B2B2B2',
+  'alkali': '#F1A9B1',
+  'transition': '#F4BF79',
+  'alkaline earth': '#7A47A7',
+  'post-transition': '#F7C46A',
+  'actinide': '#7CDD6B',
+  'lanthanide': '#B7D358',
+  'metalloid': '#70B843',
+  'other nonmetals': '#C6D0D5',
+  'noble gas': '#F06F6E',
+}
