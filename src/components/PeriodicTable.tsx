ac6060f9d42a23b383f00e44eb8ececbdfbb1709
Carlos Martinez <itscarlosisaac@gmail.com>
2020-01-06 12:02:37 -0400

    Updated component to stateless.

--- src/components/PeriodicTable.tsx
+++ src/components/PeriodicTable.tsx
@@ -1,12 +1,8 @@
 import React, {Component, Fragment, useState, useEffect} from "react";
 import Header from './molecules/Header';
 import Table from './molecules/Table';
+import Details from './molecules/Details';
 import { MessageHub, EventType } from '../utils/MessageHub';
-
-enum Theme {
-    dark = 0,
-    light = 1,
-}
 
 const PeriodicTable = (props: any) => {
     const [ theme, setTheme ] = useState(1);
@@ -22,6 +18,7 @@ const PeriodicTable = (props: any) => {
 
     return (
         <Fragment>
+            <Details />
             <Header theme={theme} />
             <Table theme={theme} />
         </Fragment>
@@ -29,38 +26,3 @@ const PeriodicTable = (props: any) => {
 }
 
 export default PeriodicTable;
-
-// export class PeriodicTable extends Component<any, any> {
-//     constructor(props: any){
-//         super(props);
-//         this.state = {
-//             theme: Theme.light,
-//         }
-//     }
-
-//     componentWillMount() {
-//       console.log(this.state)
-      
-//     }
-
-//     componentDidMount() {
-//       MessageHub.addListener(EventType.Toggle, this.handleThemeToggle);
-//       const [ data, isLoading ] = useHttp("https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json");
-
-//       console.log(data, isLoading)
-//     }
-
-//     handleThemeToggle = (data: number) => {
-//         this.setState(() => ({ theme: data }));
-//     }
-
-//     render() {
-//         const { theme } = this.state
-//         return (
-//             <Fragment>
-//                 <Header theme={theme} />
-//                 <Table theme={theme} />
-//             </Fragment>
-//         )
-//     }
-// }
